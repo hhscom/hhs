eb998274d4ac0af09f55bb712d07ce6df548b73c
hhs <1145238302qq.com>
2022-11-23 21:55:24 +0800

    第一次提交

--- src/main/java/com/hhs/TestGit.java
+++ src/main/java/com/hhs/TestGit.java
@@ -3,5 +3,6 @@ package com.hhs;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hot-fix分支修改了");
     }
 }
